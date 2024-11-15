839c5c4026bcf66d2eeed79693d131a7822f6b89
cwils163 <cwils163@vols.utk.edu>
2024-11-15 00:04:58 -0500

    adding commented out route.ts file

---
+++ src/app/api/events/route.ts
@@ -0,0 +1,11 @@
+/*import { getAllEvents } from '@/server/actions/event';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const events = await getAllEvents();
+    return NextResponse.json(events, { status: 200 });
+  } catch (error) {
+    return error;
+  }
+}*/
