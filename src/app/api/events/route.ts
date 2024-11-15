2e233ad3f9cdec706f5bd462e95efd9e81076de1
LSmith2174 <113145914+LSmith2174@users.noreply.github.com>
2024-11-15 00:24:49 -0500

    Updated events api and mongodb database to get get all events working

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,11 +1,11 @@
-/*import { getAllEvents } from '@/server/actions/event';
+import { getAllEvents } from '@/server/actions/event';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(): Promise<NextResponse> {
   try {
     const events = await getAllEvents();
     return NextResponse.json(events, { status: 200 });
   } catch (error) {
-    return error;
+    return NextResponse.json({ message: 'Unknown Error' }, { status: 500 });
   }
-}*/
+}
