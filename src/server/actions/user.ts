5f4fc468feca436569b5b3419b85a9db8efa5489
cwils163 <cwils163@vols.utk.edu>
2025-01-27 14:11:23 -0500

    added ability to update users

--- src/server/actions/user.ts
+++ src/server/actions/user.ts
@@ -1,6 +1,6 @@
 import dbConnect from '@/utils/db-connect';
 import UserSchema from '@/server/models/user';
-import { UserResponse, UserRequest } from '@/types/user';
+import { UserResponse, UserRequest, UpdateUserRequest } from '@/types/user';
 import { isValidObjectId } from 'mongoose';
 
 export async function getAllUsers(): Promise<UserResponse[]> {
@@ -40,5 +40,55 @@ export async function deleteUser(userId: string): Promise<void> {
     } catch (error) {
       throw error;
     }
+  }
+}
+
+export async function updateUser(
+  userId: string,
+  updatedUser: UpdateUserRequest
+): Promise<void> {
+  if (!isValidObjectId(userId)) {
+    throw new Error('bad user id');
+  }
+
+  if (!updatedUser || Object.keys(updatedUser).length === 0) {
+    throw new Error('bad UpdateUserRequest');
+  }
+
+  let res;
+  try {
+    await dbConnect();
+    res = await UserSchema.findByIdAndUpdate(userId, updatedUser);
+  } catch (error) {
+    throw error;
+  }
+
+  if (!res) {
+    throw new Error();
+  }
+}
+
+export async function updateUserAction(
+  userId: string,
+  userUpdatesRequest: UpdateUserRequest
+): Promise<void> {
+  if (!isValidObjectId(userId)) {
+    throw new Error('bad user id');
+  }
+
+  if (!userUpdatesRequest || Object.keys(userUpdatesRequest).length === 0) {
+    throw new Error('bad UpdateUserRequest');
+  }
+
+  let res;
+  try {
+    await dbConnect();
+    res = await UserSchema.findByIdAndUpdate(userId, userUpdatesRequest);
+  } catch (error) {
+    throw error;
+  }
+
+  if (!res) {
+    throw new Error();
   }
 }
