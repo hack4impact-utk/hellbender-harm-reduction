2fb670424d871a3ede3d6dcf9591aab4a0716eca
bbelandr <bbelandr@vols.utk.edu>
2025-02-23 18:00:00 -0500

    Add the PUT API endpoint

--- src/app/api/events/eventId/route.ts
+++ src/app/api/events/eventId/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { zObjectId } from '@/types/objectId';
-import { getEvent } from '@/server/actions/event';
+import { getEvent, updateEvent } from '@/server/actions/event';
+import { zUpdateEventRequest } from '@/types/event';
 
 export async function GET(
   _request: NextRequest,
@@ -17,4 +18,34 @@ export async function GET(
     return NextResponse.json({ message: 'User not found ' }, { status: 404 });
   }
   return NextResponse.json(response, { status: 200 });
+}
+
+export async function PUT(request: NextRequest, {params}: {params: {eventId:string}}) {
+  // Checking the data for bad formats
+  const formId = params.eventId;
+  const idValidationResult = zObjectId.safeParse(formId);
+  if (!idValidationResult.success) {
+    return NextResponse.json({ message: 'invalid id' }, {status: 400});
+  }
+
+  const requestValidationResult = zUpdateEventRequest.safeParse(request);
+  if (!requestValidationResult) {
+    return NextResponse.json({ message: 'bad request'}, {status: 400}); // This wasn't explicitly stated in the story, but I've added this because I'm pretty sure it needs to be in here
+  }
+  
+  try {
+    const stuffToChange = await request.json(); // Turning the request into a readable json file
+    const updatedEvent = await updateEvent(params.eventId, stuffToChange);
+    
+    if (!updatedEvent) {
+      return NextResponse.json({error: 'Item not found'}, {status: 404});
+    }
+  }
+  catch (error) {
+    const message = error instanceof Error ? error.message : 'Unknown error';
+    return NextResponse.json(
+      {error: 'Failed to update item information', details: message },
+      {status: 500}
+    );
+  }
 }
