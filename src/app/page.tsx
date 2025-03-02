dfa7b3f0c1de4368812858ee806c5aba48237691
bbelandr <bbelandr@vols.utk.edu>
2025-03-02 17:23:31 -0500

    undoing some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import { BasicInfo } from '@/components/ProfilePage/index'
-import { getUser } from '@/server/actions/user';
-
 export default async function Home() {
-  const user = await getUser("67bc854cecd286e252e1a54d"); // Changeme
-  if (!user) {
-    return;
-  }
   return (
     <div>
       <h1>Home page</h1>
-      <BasicInfo name={user.name} pronouns={user.pronouns} profilePicture={user.image} />
     </div>
   );
 }
