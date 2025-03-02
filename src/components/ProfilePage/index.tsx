d8c758a2549532329b472e53e3022ed2b407007f
bbelandr <bbelandr@vols.utk.edu>
2025-03-02 14:55:56 -0500

    Centering the component and correctly fetching data from props

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,3 +1,18 @@
-export function BasicInfo() {
-    return <h2>Sup, dumbass</h2>;
+import { Avatar, TextField, Stack } from "@mui/material";
+
+interface basicInfoProps {
+    profilePicture: string,
+    name: string,
+    pronouns: string
+}
+
+// Uses a stack for natural vertical formatting
+export function BasicInfo(props: basicInfoProps) {
+    return (
+        <Stack spacing={2} alignItems="center">
+            <Avatar alt={props.name} src={props.profilePicture} sx={{ width:200, height:200,}} />
+            <TextField disabled id="outlined-basic" defaultValue={props.name} inputProps={{min: 0, style: { textAlign: 'center' }}} />
+            <TextField disabled id="outlined-basic" defaultValue={props.pronouns} inputProps={{min: 0, style: { textAlign: 'center' }}} />
+        </Stack>
+    );
 }
