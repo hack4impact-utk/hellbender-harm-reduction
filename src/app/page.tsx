0a68ed5f81f3a97084c084237e0123b9b3ced13d
bbelandr <bbelandr@vols.utk.edu>
2025-03-02 14:56:55 -0500

    Example test for this branch if you're interested in seeing what this looks like

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import {BasicInfo} from '@/components/ProfilePage/index'
+import { getUser } from '@/server/actions/user';
+
 export default async function Home() {
+  const user = await getUser("673a4e39f0a55dbb5fc5e0b4");
+  if (!user) {
+    return;
+  }
   return (
     <div>
       <h1>Home page</h1>
+      <BasicInfo name={user.name} pronouns={user.pronouns} profilePicture={user.picture} />
     </div>
   );
 }
