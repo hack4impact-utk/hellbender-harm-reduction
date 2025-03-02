2866e9fb119eef7147a6c995661a34c5a801c7e0
bbelandr <bbelandr@vols.utk.edu>
2025-03-02 17:02:49 -0500

    comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import { BasicInfo } from '@/components/ProfilePage/index'
 import { getUser } from '@/server/actions/user';
 
 export default async function Home() {
-  const user = await getUser("673a4e39f0a55dbb5fc5e0b4");
+  const user = await getUser("67bc854cecd286e252e1a54d"); // Changeme
   if (!user) {
     return;
   }
   return (
     <div>
       <h1>Home page</h1>
-      <BasicInfo name={user.name} pronouns={user.pronouns} profilePicture={user.picture} />
+      <BasicInfo name={user.name} pronouns={user.pronouns} profilePicture={user.image} />
     </div>
   );
 }
