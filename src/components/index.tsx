df632cd65ab1129935947d98662535f13bf8c269
bbelandr <bbelandr@vols.utk.edu>
2025-03-05 20:01:00 -0500

    comments

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client'    // Makes the client use their resources to render the component instead of having the server render it first (epic)
 import { TextField, Stack } from "@mui/material";
 
 interface AccountInfoProps {
@@ -8,10 +8,10 @@ interface AccountInfoProps {
 
 export function AccountInfo(props: AccountInfoProps) {
     return (
-    <Stack spacing={2}>
-        <h3>Email</h3>
-        <TextField disabled defaultValue={props.email} />
-        <h3>Phone Number</h3>
-        <TextField disabled defaultValue={props.phone} />
-    </Stack>)
+        <Stack spacing={2}>
+            <h3>Email</h3>
+            <TextField disabled defaultValue={props.email} />
+            <h3>Phone Number</h3>
+            <TextField disabled defaultValue={props.phone} />
+        </Stack>)
 }
