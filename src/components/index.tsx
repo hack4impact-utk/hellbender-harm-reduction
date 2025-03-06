3a1e428e6e1ce8a942148a360ddf1bac5045c69d
bbelandr <bbelandr@vols.utk.edu>
2025-03-05 19:48:45 -0500

    basic layout

---
+++ src/components/index.tsx
@@ -0,0 +1,16 @@
+import { TextField, Stack } from "@mui/material";
+
+interface AccountInfoProps {
+    email: string,
+    phone: string
+}
+
+function AccountInfo(props: AccountInfoProps) {
+    return (
+    <Stack spacing={2}>
+        <h3>Email</h3>
+        <TextField disabled defaultValue={props.email} />
+        <h3>Phone Number</h3>
+        <TextField disabled defaultValue={props.phone} />
+    </Stack>)
+}
