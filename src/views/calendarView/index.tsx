8c84e2c7a21b7aa7532680e12f2d6495ae05db30
cwils163 <cwils163@vols.utk.edu>
2025-03-16 18:04:47 -0400

    saving more calendar changes

--- src/views/calendarView/index.tsx
+++ src/views/calendarView/index.tsx
@@ -1,10 +1,17 @@
+'use client';
 import NavBar from '@/components/navbar';
 import { Box } from '@mui/material';
 import CalendarComp from '@/components/usercalendar';
+import hhrColors from '@/utils/hhr-theme';
+import { alpha } from '@mui/material/styles';
 
 export default function CalendarView() {
   return (
-    <Box>
+    <Box
+      sx={{
+        backgroundColor: alpha(hhrColors.palette.hhr.light, 1),
+      }}
+    >
       {/* Top Navigation Bar */}
       <NavBar></NavBar>
       <CalendarComp />
