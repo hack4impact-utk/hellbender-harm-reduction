1ecc0cfc884c3ba78ecfb4ca0235d52a6255cde9
bbelandr <bbelandr@vols.utk.edu>
2025-04-06 16:54:21 -0400

    Changing default h3 to a typography

--- src/components/accountinfo.tsx
+++ src/components/accountinfo.tsx
@@ -1,5 +1,5 @@
 'use client'; // Makes the client use their resources to render the component instead of having the server render it first (epic)
-import { TextField, Stack } from '@mui/material';
+import { TextField, Stack, Typography } from '@mui/material';
 
 interface AccountInfoProps {
   email: string;
@@ -9,9 +9,9 @@ interface AccountInfoProps {
 export function AccountInfo(props: AccountInfoProps) {
   return (
     <Stack spacing={2}>
-      <h3>Email</h3>
+      <Typography variant="h3"> Email </Typography>
       <TextField disabled defaultValue={props.email} />
-      <h3>Phone Number</h3>
+      <Typography variant="h3"> Phone Number </Typography>
       <TextField disabled defaultValue={props.phone} />
     </Stack>
   );
