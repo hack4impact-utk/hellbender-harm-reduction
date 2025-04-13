dd3a162ba9c59002514794c941e5a510f716fc71
Vincent Guo <152289662+guo-vincent@users.noreply.github.com>
2025-04-13 17:38:27 -0400

    Removed Extraneous div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,7 @@ import SignInUpView from '@/views/signInUpView';
 export default async function Home() {
   return (
     <div>
-      <div>
-        <SignInUpView
-          _form={<SignInForm email="" password="" />}
-        ></SignInUpView>
-      </div>
+      <SignInUpView _form={<SignInForm email="" password="" />}></SignInUpView>
     </div>
   );
 }
