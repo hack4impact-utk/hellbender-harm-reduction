77895c3586d8a2270e61a72b8c029fbfbfd7d4b1
bbelandr <bbelandr@vols.utk.edu>
2025-04-13 16:57:10 -0400

    Creating the interface layout

--- src/components/accountinfo.tsx
+++ src/components/accountinfo.tsx
@@ -1,18 +1,49 @@
 'use client'; // Makes the client use their resources to render the component instead of having the server render it first (epic)
-import { TextField, Stack, Typography } from '@mui/material';
+import { TextField, Stack, Typography, Grid } from '@mui/material';
+
+interface Tag {
+  tagName: string;
+  cartPicture: string;
+  certification: boolean;
+}
+
+enum tagProfEnum {
+  Beginner,
+  Intermediate, 
+  Expert,
+  NotApplicable,
+}
+
+interface userTag {
+  tag: Tag;
+  tagProf: tagProfEnum;
+}
 
 interface AccountInfoProps {
   email: string;
   phone: string;
+  tags:  userTag[];
 }
 
 export function AccountInfo(props: AccountInfoProps) {
   return (
-    <Stack spacing={2}>
-      <Typography variant="h5"> Email </Typography>
-      <TextField disabled defaultValue={props.email} />
-      <Typography variant="h5"> Phone Number </Typography>
-      <TextField disabled defaultValue={props.phone} />
-    </Stack>
+    <div>
+      <Stack spacing={2}>
+        <Typography variant="h5"> Email </Typography>
+        <TextField disabled defaultValue={props.email} />
+        <Typography variant="h5"> Phone Number </Typography>
+        <TextField disabled defaultValue={props.phone} />
+      </Stack>
+      <Grid container spacing={2}>
+        
+        <Grid size={6}>
+          <Accordion>
+            <AccordionSummary expandIcon={<ArrowDownwardIcon />} aria-controls="tag-information">
+
+            </AccordionSummary>
+          </Accordion>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
