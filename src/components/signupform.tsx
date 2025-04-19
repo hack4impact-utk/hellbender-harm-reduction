435435fda401f0dda630158d9abeee5ca654f9c3
alay10 <alay10@vols.utk.edu>
2025-04-19 18:43:12 -0400

    Added back and forward button and aligned them

--- src/components/signupform.tsx
+++ src/components/signupform.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
 import { SignUpBasicInfo } from './signupbasicinfo';
-import { Button } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 import { SignUpContactInfo } from './signupcontactinfo';
 import { AddLang } from './addlang';
 import { SetEventPref } from './seteventpref';
@@ -10,6 +10,7 @@ import { SetNewEventNotif } from './setneweventnotif';
 import { CertificationInfo } from './certificationinfo';
 import { AddAccommodations } from './addaccommodation';
 import { SignUpReferral } from './signupreferral';
+import { NavigateBefore, NavigateNext } from '@mui/icons-material';
 // import { SetReminders } from './setreminders';
 
 export enum FormEnum {
@@ -24,43 +25,78 @@ export function SignUpInfoForm() {
   const [currentForm, setCurrentForm] = useState<FormEnum>(FormEnum.BasicInfo);
 
   return (
-    <div>
-      {currentForm === FormEnum.BasicInfo && (
-        <div>
-          <SignUpBasicInfo></SignUpBasicInfo>
-          <SignUpContactInfo></SignUpContactInfo>
-        </div>
-      )}
-      {currentForm === FormEnum.Emergency && (
-        <div>
-          <p>Emergency Contact</p>
-          <AddAccommodations></AddAccommodations>
-        </div>
-      )}
-      {currentForm === FormEnum.LanguageInfo && (
-        <AddLang Languages={['Spanish']} UserTags={[null]}></AddLang>
-      )}
-      {currentForm === FormEnum.EventPreferences && (
-        <div>
-          <SetEventPref></SetEventPref>
-          <SetNewEventNotif></SetNewEventNotif>
-          {/*<SetReminders></SetReminders>*/}
-        </div>
-      )}
-      {currentForm === FormEnum.CertificationInfo && (
-        <div>
-          <CertificationInfo
-            data={[{ certName: '', certDescription: '' }]}
-          ></CertificationInfo>
-          <SignUpReferral></SignUpReferral>
-        </div>
-      )}
-      <Button
-        onClick={() => setCurrentForm((currentForm + 1) % 5)}
-        variant="contained"
+    <Box display="flex" flexDirection="column" height="95%">
+      <Box
+        pb={3}
+        sx={{
+          flexGrow: 1,
+          overflow: 'auto',
+          pr: 2,
+        }}
       >
-        Next
-      </Button>
-    </div>
+        {currentForm === FormEnum.BasicInfo && (
+          <div>
+            <SignUpBasicInfo></SignUpBasicInfo>
+            <Box mt={2}></Box>
+            <SignUpContactInfo></SignUpContactInfo>
+          </div>
+        )}
+        {currentForm === FormEnum.Emergency && (
+          <div>
+            <p>Emergency Contact</p>
+            <Box mt={2}></Box>
+            <AddAccommodations></AddAccommodations>
+          </div>
+        )}
+        {currentForm === FormEnum.LanguageInfo && (
+          <AddLang Languages={['Spanish']} UserTags={[null]}></AddLang>
+        )}
+        {currentForm === FormEnum.EventPreferences && (
+          <div>
+            <SetEventPref></SetEventPref>
+            <Box mt={2}></Box>
+            <SetNewEventNotif></SetNewEventNotif>
+            {/*<SetReminders></SetReminders>*/}
+          </div>
+        )}
+        {currentForm === FormEnum.CertificationInfo && (
+          <div>
+            <CertificationInfo
+              data={[{ certName: '', certDescription: '' }]}
+            ></CertificationInfo>
+            <Box mt={2}></Box>
+            <SignUpReferral></SignUpReferral>
+          </div>
+        )}
+      </Box>
+      <Grid container spacing={2} width={'100%'} alignSelf="center">
+        <Grid item xs={6} sm={6} display={'flex'} justifyContent={'flex-begin'}>
+          <Button
+            onClick={() => setCurrentForm((currentForm - 1) % 5)}
+            variant="contained"
+            color="primary"
+            startIcon={<NavigateBefore />}
+            sx={{
+              mt: 2,
+            }}
+          >
+            Back
+          </Button>
+        </Grid>
+        <Grid item xs={6} sm={6} display={'flex'} justifyContent={'flex-end'}>
+          <Button
+            onClick={() => setCurrentForm((currentForm + 1) % 5)}
+            variant="contained"
+            color="primary"
+            endIcon={<NavigateNext />}
+            sx={{
+              mt: 2,
+            }}
+          >
+            Next
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
