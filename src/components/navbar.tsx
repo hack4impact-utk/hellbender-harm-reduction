08f304c896c17254ffe3e55c768509e7a5e5489a
LSmith2174 <113145914+LSmith2174@users.noreply.github.com>
2025-04-21 04:46:55 -0400

    Fixed er up real good

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,52 +5,122 @@ import hhrColors from '@/utils/hhr-theme';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Image from 'next/image';
 import logo from '/public/hellbender_logo_2color_1668738125.png';
+import { alpha } from '@mui/material/styles';
+interface NavBarProps {
+  userType: string;
+  userId: string;
+  page: string;
+}
 
-export default function Topbar() {
+export default function NavBar({ userType, userId, page }: NavBarProps) {
   return (
     <AppBar
       sx={{
         width: '100%',
-        height: '9vh',
-        backgroundColor: hhrColors.palette.hhr.main,
+        height: '75px',
+        backgroundColor: alpha(hhrColors.palette.hhr.main, 0.75),
         position: 'static',
         top: 0,
         left: 0,
         right: 0,
         display: 'flex',
         justifyContent: 'center',
-        p: 2, // padding for spacing
+        p: 2,
       }}
     >
       <Toolbar>
         <Grid container alignItems="center" justifyContent="space-between">
-          <Grid item xs={4} container alignItems="flex-start">
+          {/* Logo */}
+          <Grid
+            item
+            xs={6}
+            sm={4}
+            md={6}
+            container
+            alignItems="center"
+            columnGap={2}
+          >
             <IconButton
+              component={Link}
+              href="https://www.hellbenderharmreduction.org/"
               color="inherit"
               aria-label="logo"
-              sx={{ padding: 0, height: '7vh', aspectRatio: '1:1' }}
+              sx={{
+                padding: 0,
+                height: '60px',
+                aspectRatio: '1:1',
+              }}
             >
               <Image
                 alt="logo"
                 src={logo}
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-              ></Image>
+                style={{ width: '100%', height: '100%' }}
+              />
             </IconButton>
-          </Grid>
-          <Grid item xs={8} container justifyContent="flex-end">
             <Button
               color="inherit"
               component={Link}
-              href="/calendar"
+              href={`/dashboard/${userId}`}
               sx={{
                 fontWeight: 'bold',
-                fontSize: 40,
-                paddingRight: 8,
+                fontSize: 30,
+                paddingRight: 4,
+                paddingLeft: 4,
                 justifyContent: 'center',
-                alignSelf: 'center',
+                backgroundColor:
+                  page === 'Dashboard' ? alpha('#000000', 0.1) : 'transparent',
+                '&:hover': { backgroundColor: alpha('#ffffff', 0.25) },
+              }}
+            >
+              DASHBOARD
+            </Button>
+            {/* Volunteer button (conditional) */}
+            {(userType === 'Admin' || userType === 'Owner') && (
+              <Button
+                color="inherit"
+                component={Link}
+                href={`/volunteers/${userId}`}
+                sx={{
+                  fontWeight: 'bold',
+                  fontSize: 30,
+                  paddingRight: 4,
+                  paddingLeft: 4,
+                  justifyContent: 'center',
+                  backgroundColor:
+                    page === 'Volunteers'
+                      ? alpha('#000000', 0.1)
+                      : 'transparent',
+                  '&:hover': { backgroundColor: alpha('#ffffff', 0.25) },
+                }}
+              >
+                VOLUNTEERS
+              </Button>
+            )}
+          </Grid>
+          {/* Calendar, events, and profile */}
+          <Grid
+            item
+            xs={6}
+            sm={4}
+            md={6}
+            container
+            alignItems="center"
+            justifyContent={'flex-end'}
+            columnGap={2}
+          >
+            <Button
+              color="inherit"
+              component={Link}
+              href={`/calendar/${userId}`}
+              sx={{
+                fontWeight: 'bold',
+                fontSize: 30,
+                paddingRight: 4,
+                paddingLeft: 4,
+                justifyContent: 'center',
+                backgroundColor:
+                  page === 'Calendar' ? alpha('#000000', 0.1) : 'transparent',
+                '&:hover': { backgroundColor: alpha('#ffffff', 0.25) },
               }}
             >
               CALENDAR
@@ -58,18 +128,34 @@ export default function Topbar() {
             <Button
               color="inherit"
               component={Link}
-              href="/events"
+              href={`/events/${userId}`}
               sx={{
                 fontWeight: 'bold',
-                fontSize: 40,
-                paddingRight: 8,
+                fontSize: 30,
+                paddingRight: 4,
+                paddingLeft: 4,
                 justifyContent: 'center',
+                backgroundColor:
+                  page === 'Events' ? alpha('#000000', 0.1) : 'transparent',
+                '&:hover': { backgroundColor: alpha('#ffffff', 0.25) },
               }}
             >
               EVENTS
             </Button>
-            <IconButton edge="end" color="inherit" aria-label="profile">
-              <AccountCircleIcon sx={{ fontSize: 80 }} />
+
+            <IconButton
+              component={Link}
+              href={`/profile/${userId}`}
+              edge="end"
+              color="inherit"
+              aria-label="profile"
+              sx={{
+                backgroundColor:
+                  page === 'Profile' ? alpha('#000000', 0.1) : 'transparent',
+                '&:hover': { backgroundColor: alpha('#ffffff', 0.25) },
+              }}
+            >
+              <AccountCircleIcon sx={{ fontSize: 50 }} />
             </IconButton>
           </Grid>
         </Grid>
