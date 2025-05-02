e0ce9df2a569f3d181ab832098deda2fcbaf71dd
bbelandr <bbelandr@vols.utk.edu>
2025-05-02 12:07:09 -0400

    functionality n stuff

--- src/components/accountinfo.tsx
+++ src/components/accountinfo.tsx
@@ -25,6 +25,15 @@ interface AccountInfoProps {
 }
 
 export function AccountInfo(props: AccountInfoProps) {
+  const normalTags = props.tags;
+  const priorityTags: userTag[] = [];
+  for (let i = 0; i < normalTags.length; i++) {
+    if (normalTags[i].tag.tagName === "Hellbender Harm Reduction 101" || normalTags[i].tag.tagName === "Bloodborne Pathogens Training") {
+      priorityTags.push(normalTags[i]);
+      normalTags.splice(i, 1); // Remove the tag from the original array
+      i--; // Updating where we are in the array
+    }
+  }
   return (
     <div>
       <Stack spacing={2}>
@@ -35,11 +44,41 @@ export function AccountInfo(props: AccountInfoProps) {
 
       </Stack>
       <Grid container spacing={2}>
-        {props.tags.map((userTag) => {
+      {priorityTags.map((userTag) => {
+          let tagColor;
+          if (userTag.tagProf !== "N/A") {
+            tagColor = "#808080";   // The user has the tag, set the color to grey
+          }
+          else {
+            tagColor = "#ffffff";   // The user does not have the tag, set the color to white
+          }
           return (
             <Grid item xs={6} key={userTag.tag.tagName}>
               <Box sx={
                 {
+                  backgroundColor: tagColor,
+                  border: 1,
+                  borderRadius: 2
+                }
+              }>
+                <Typography variant="h6" align="center"> {userTag.tag.tagName}: {userTag.tagProf} </Typography>
+              </Box>
+            </Grid>
+          );
+        })}
+        {normalTags.map((userTag) => {
+          let tagColor;
+          if (userTag.tagProf !== "N/A") {
+            tagColor = "#808080";   // The user has the tag, set the color to grey
+          }
+          else {
+            tagColor = "#ffffff";   // The user does not have the tag, set the color to white
+          }
+          return (
+            <Grid item xs={4} key={userTag.tag.tagName}>
+              <Box sx={
+                {
+                  backgroundColor: tagColor,
                   border: 1,
                   borderRadius: 2
                 }
